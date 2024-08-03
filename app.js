a5e3891c65e85989c5bcdaa8adde5e829d8872d0
Lana Maugé <lanamauge@gmail.com>
2024-08-03 20:34:04 +0100

    installed Express

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import pkg from 'pg';
+import express from 'express';
+
+
 const { Pool } = pkg;
 
 const connectionString = "postgresql://playlist_vh9f_user:3CxgBlAr9lKnwTscVsVtGFZ9KxB9yMFS@dpg-cqn32p88fa8c73aktodg-a.frankfurt-postgres.render.com/playlist_vh9f";
@@ -29,3 +32,16 @@ async function queryDatabase(query) {
 }
 
 queryDatabase('SELECT * FROM songs WHERE id = 2');
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
